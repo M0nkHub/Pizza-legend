268544f3b1e3547293d491698722108f8e30535f
M0nkHub <179734551+M0nkHub@users.noreply.github.com>
2024-11-26 00:08:48 +0100

    Initial commit

---
+++ Overworld.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: "Spongebob",
+  lastName: "squarepants",
+  age: 30,
+  isEmployed: true,
+};
+
+console.log("hi i am ", 3, person.firstName);
+console.log(person.age);
+
+function Car(make, model, year, color) {}
